00aec823e3900a8c606cc0898fa28dbe87677d9b
Patrick Sidharta <prdsidharta@gmail.com>
2026-06-28 21:02:34 -0400

    feat: implement color mask for object identification and add text to window

--- client/vision.py
+++ client/vision.py
@@ -2,7 +2,11 @@ import grpc
 import outfit_pb2
 import outfit_pb2_grpc
 import cv2
+
 import sys
+import numpy as np
+import time
+
 
 vid = cv2.VideoCapture(0)
 channel = grpc.insecure_channel('localhost:50051')
@@ -13,12 +17,50 @@ if not vid.isOpened():
     sys.exit()
 print("Camera on. Press 'q' to quit.")
 
+last_call = 0
+cd_sec = 3.0
+cache_data = None
+
 while True:
     ret, frame = vid.read()
-
     if not ret:
             print ("Lost camera connection")
             break
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([136, 87, 111])
+    upper_red = np.array([180, 255, 255])
+    red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
+
+    contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+         area = cv2.contourArea(contour)
+
+         if area > 8000:
+              x, y, w, h = cv2.boundingRect(contour)
+              cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 3)
+
+              cur_time = time.time()
+              if (cur_time - last_call) > cd_sec:
+                   try:
+                        request = outfit_pb2.ItemRequest(item_id = 0)
+                        response = client.GetFitInfo(request)
+
+                        cache_data = {
+                             "name": response.clothes_name,
+                             "brand": response.brand_name,
+                             "price": f"${response.price:.2f}"
+                        }
+                        last_call = cur_time
+                   except grpc.RpcError:
+                        print("C++ Server Offline.")
+                        cache_data = None
+              if cache_data:
+                display_text = f"{cache_data['brand']} {cache_data['name']} - {cache_data['price']}"
+                cv2.putText(frame, display_text, (x, y - 10),
+                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                break
+
     cv2.imshow("Outfit Analyzer", frame)
 
     key = cv2.waitKey(1) & 0xFF
